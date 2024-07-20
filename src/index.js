3c2857e62d595c1c3f9cc68c6f157fe5cdf480af
hhw <1918697599@qq.com>
2024-07-20 17:56:54 +0800

    配置路由

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
+import { HashRouter } from 'react-router-dom'
 
 import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
   </React.StrictMode>
 )
